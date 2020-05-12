be4ab884691833403bcfb45a38f2fa2d0d5ad72c
SSep <ssep4u@gmail.com>
2020-05-12 21:35:29 +0900

    urls detail/<int:pk>/

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import BookmarkList, BookmarkCreateView
+from .views import BookmarkList, BookmarkCreateView, BookmarkDetailView
 
 app_name = 'bookmark'
 
 urlpatterns = [
   path('', BookmarkList.as_view(), name='list'),
   path('add/', BookmarkCreateView.as_view(), name='add'),
+  path('detail/<int:pk>/', BookmarkDetailView.as_view(), name='detail'),
 ]
