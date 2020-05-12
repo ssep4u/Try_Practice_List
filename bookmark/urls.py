a48b33cd9b8ac457397965c3cf30076af3df86c1
SSep <ssep4u@gmail.com>
2020-05-12 21:28:21 +0900

    urls add/

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import BookmarkList
+from .views import BookmarkList, BookmarkCreateView
 
 app_name = 'bookmark'
 
 urlpatterns = [
   path('', BookmarkList.as_view(), name='list'),
+  path('add/', BookmarkCreateView.as_view(), name='add'),
 ]
